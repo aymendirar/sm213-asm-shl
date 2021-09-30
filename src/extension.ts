f26588c95638ba47684c0f4b7cb44c5ccaa2f175
friendseeker <66892505+Friendseeker@users.noreply.github.com>
2021-09-29 18:54:41 -0700

    refactor(extensions.ts): DEFN -> OPCODES

--- src/extension.ts
+++ src/extension.ts
@@ -27,41 +27,99 @@ const HOVER_DEFN = vscode.languages.registerHoverProvider({ scheme: 'file', lang
         	const word = document.getText(range);
 		// const hoverMsg: vscode.MarkdownString = new vscode.MarkdownString().appendText('GTFO!');
 		// hoverMsg.isTrusted = true;
-		if (word in DEFN) {return new vscode.Hover(DEFN[word]);}
+		if (isOpcodeName(word) && OPCODES.has(word)) {
+			return new vscode.Hover(OPCODES.get(word)!.markdownDesc);
+		}
 		// return new vscode.Hover(hoverMsg);
 	}
 });
 
-const DEFN: {[key: string]: vscode.MarkdownString} = {
-	
-	ld : opcodeDesc('ld', 'load from memory'),
-	st: opcodeDesc('st','store into memory'),
-	mov: opcodeDesc('mov','move between registers'),
-	add: opcodeDesc('add', 'add integers'),
-	and: opcodeDesc('and','bitwise logical and of two 32-bit values'),
-	inc: opcodeDesc('inc', 'increment an integer'),
-	inca: opcodeDesc('inca','add four to an integer (increment word address)'),
-	dec: opcodeDesc('dec','decrement an integer'),
-	deca: opcodeDesc('deca','subtract four from integer (decrement word address)'),
-	not: opcodeDesc('not','bitwise compliment (i.e., not) of 32-bit value'),
-	gpc: opcodeDesc('gpc','get value of program counter'),
-	shr: opcodeDesc('shr', 'shift right'),
-	shl: opcodeDesc('shl','shift left'),
-	br: opcodeDesc('br','unconditional branch'),
-	beq: opcodeDesc('beq', 'branch when equal to zero'),
-	bgt: opcodeDesc('bgt','branch when greater than zero'),
-	j: opcodeDesc('j','unconditional jump'),
-	halt : opcodeDesc('halt', 'stop processor'),
-	nop : opcodeDesc('nop', 'do nothing'),
+type AddressingMode = 'immediate' | 'register' | 'base+offset' | 'indexed';
+
+const opcodeNames = ['ld', 'st', 'mov', 'add', 'and', "inc", "inca"
+		,'dec', 'deca', 'not', 'gpc', 'shr', 'shl'
+		,'br', 'beq', 'bgt', 'j', 'halt', 'nop'] as const;
+
+type OpcodeName = typeof opcodeNames[number];
+function isOpcodeName(x: string): x is OpcodeName {
+	// widen formats to string[] so indexOf(x) works
+	return (opcodeNames as readonly string[]).indexOf(x) >= 0;
+}
+
+type Signature = {
+	arg1?: Argument;
+	arg2?: Argument;
 };
 
-function opcodeDesc(opcode: string, desc: string) {
-	// desc is a short (<10 word) description
-	// taken from page 19 of companion
-	return new vscode.MarkdownString()
-		.appendCodeblock(opcode, 'asm')
-		.appendText(`\n`)
-		.appendMarkdown(desc);
+class Argument {
+	argName: string;
+	addressingModes: AddressingMode[];
+	desc: string;
+	constructor(argName: string, modes: AddressingMode[]) {
+		this.argName = argName;
+		this.addressingModes = modes;
+		// Update desc
+		// in the format of <argName: mode1/mode2/mode3>
+		this.desc = `<${this.argName}: `;
+		for (const mode of modes) {
+			this.desc += `${mode}/`;
+		}
+		this.desc = this.desc.replace(/.$/,">");
+	}
 };
+
+class Opcode {
+	name: OpcodeName;
+	signature?: Signature;
+	// TODO: add TsDoc for description as it is not a too obvious one.
+	description: string;
+	markdownDesc: vscode.MarkdownString;
+	constructor(name: OpcodeName, description: string, signature?: Signature) {
+		this.name = name;
+		this.signature = signature;
+		this.description = description;
+		// Update description from signature
+		if (this.signature?.arg1) {
+			this.description = this.description.replace('$1', String.raw`\<${this.signature.arg1.argName}\>`);
+		}
+		if (this.signature?.arg2) {
+			this.description = this.description.replace('$2', String.raw`\<${this.signature.arg2.argName}\>`);
+		}
+
+		// Update markdownDesc
+		let heading = this.name;
+		if (this.signature?.arg1) {
+			heading += ` ${this.signature?.arg1.desc}`;
+		}
+		if (this.signature?.arg2) {
+			heading += ` ${this.signature?.arg2.desc}`;
+		}
+		this.markdownDesc = new vscode.MarkdownString()
+			.appendCodeblock(heading, 'asm') 
+			// if it doesn't format properly, change above to appendText()
+			.appendText(`\n`) // TODO: Try change it to appendMarkdown(<hr>)?
+			.appendMarkdown(this.description);	
+	}
+}
+
+const OPCODES = new Map<OpcodeName, Opcode>([
+	['ld', new Opcode('ld', 'loads value from $1 to $2',
+			 {arg1: new Argument('val', ['immediate','base+offset','indexed']),
+			arg2: new Argument('dest', ['register'])})],
+	['st', new Opcode('st', 'stores value from register $1 to memory address $2',
+			{arg1: new Argument('source', ['register']),
+			arg2: new Argument('dest', ['base+offset', 'indexed'])})],
+	['halt', new Opcode('halt', 'stops execution')],
+	['mov', new Opcode('mov', 'moves value from register $1 to $2',
+			{arg1: new Argument('source', ['register']),
+			arg2: new Argument('dest', ['register'])})],
+	['nop', new Opcode('nop', 'no operation')],
+	['add', new Opcode('add', 'add integer stored in $1 and $2, stores result in $2',
+			{arg1: new Argument('val', ['register']),
+			arg2: new Argument('val', ['register'])})],
+	['and', new Opcode('and', 'performs bitwise logical and between $1 and $2, stores result in $2',
+			{arg1: new Argument('val1', ['register']),
+			arg2: new Argument('val2', ['register'])})]
+]);
 
 export function deactivate() {}
